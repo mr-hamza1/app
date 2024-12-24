d54629d70d797201e71a42a633a414fb126c9731
Your Name <you@example.com>
2024-12-24 06:18:59 +0500

    deployment

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    // Increase chunk size warning limit
-    chunkSizeWarningLimit: 1000, // Set limit to 1000 kB (optional)
-
-    // Rollup options for manual chunking
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          // Split dependencies into separate chunks
-          if (id.includes('node_modules')) {
-            if (id.includes('react')) {
-              return 'react-vendor'; // React-specific chunk
-            }
-            return 'vendor'; // General vendor chunk
-          }
-        },
-      },
-    },
-  },
+  base: '/app/', // Replace with your GitHub repo name
 })
